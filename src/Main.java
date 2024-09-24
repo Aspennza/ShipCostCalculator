0b72e94b9b174c89a5757ff0ce227116f2c942d9
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-24 08:39:58 -0400

    Program complete

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //DO FIRST IF THEN TO TEST FOR INPUT VALIDITY (double or trash)--use exit statement
-        //SECOND IF THEN IS FOR TESTING RANGE OF VALUES (less than 100, etc.)
-
 
         Scanner in = new Scanner(System.in);
         final double BREAK_OVER_PRICE = 100.00;
@@ -14,12 +11,9 @@ public class Main {
         double totalPrice = 0.00;
         String trash = "";
 
-        //output “Enter the price of your item: ”
 
         System.out.println("Enter the price of your item: ");
 
-        //input itemPrice
-        //if itemPrice >= BREAKOVER_PRICE then
         if (in.hasNextDouble()) {
             itemPrice = in.nextDouble();
             in.nextLine();
@@ -42,14 +36,5 @@ public class Main {
             System.out.println("Your shipping costs are worth " + shippingCosts + " dollars and your total price is worth " + totalPrice + " dollars.");
         }
     }
-
-    //	shippingCosts = 0.00
-    //	totalPrice = itemPrice + shippingCosts
-    //output “Your shipping costs are worth ” + shippingCosts + “ dollars and your total price is worth ” + totalPrice + “ dollars.”
-    //else
-    //	shippingCosts = itemPrice * SHIPPING_PERCENT
-    //	totalPrice = itemPrice + shippingCosts
-    //output “Your shipping costs are worth ” + shippingCosts + “ dollars and your total price is worth ” + totalPrice + “ dollars.”
-    //endIf
 }
 
